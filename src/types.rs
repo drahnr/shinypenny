be3232095f83178756009b77de20d08f3dd0ca15
Bernhard Schuster <bernhard@ahoi.io>
2020-10-28 17:04:42 +0100

    fix: must guess img fmt explicitly

--- src/types.rs
+++ src/types.rs
@@ -346,9 +346,12 @@ pub(crate) struct CompanyInfo {
 impl CompanyInfo {
     pub(crate) fn new(name: &str, address: &str, image_path: Option<PathBuf>) -> Result<Self> {
         let image = if let Some(image_path) = image_path {
+            log::trace!("Loading company image from {}", image_path.display());
             let file = fs::OpenOptions::new().read(true).open(&image_path)?;
             let reader = std::io::BufReader::with_capacity(2048, file);
-            let image = printpdf::image::io::Reader::new(reader).decode()?;
+            let reader = printpdf::image::io::Reader::new(reader).with_guessed_format()?;
+            log::trace!("Determined company image format: {:?}", reader.format());
+            let image = reader.decode()?;
             Some(image)
         } else {
             None
