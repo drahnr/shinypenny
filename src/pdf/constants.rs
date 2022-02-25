742ef3206ead6ebca2b91abfff708ccba337645d
Bernhard Schuster <bernhard@ahoi.io>
2022-02-25 11:06:51 +0100

    flip first page

--- src/pdf/constants.rs
+++ src/pdf/constants.rs
@@ -7,8 +7,8 @@ pub struct Dimensions {
 
 /// DIN A4 in mm dimensions.
 pub const DIN_A4: Dimensions = Dimensions {
-    height: Mm(210.),
-    width: Mm(297.),
+    height: Mm(297.),
+    width: Mm(210.),
 };
 
 pub static TTF_REGULAR: &'static [u8] = include_bytes!("../../assets/Roboto-Regular.ttf");
